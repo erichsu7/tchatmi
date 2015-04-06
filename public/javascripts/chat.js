c6d92be1746f60d33784fe46b27b78eefb13e58a
Eric Hsu <erichsu7@gmail.com>
2015-04-06 01:34:52 -0700

    Allow users to change rooms

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -24,6 +24,14 @@
           text: "/nick requires a nickname, i.e. /nick i<3kittens."
         })
       }
+    } else if (commands[0] === "join") {
+      if (commands[1]) {
+        this.socket.emit("roomChangeRequest", commands[1])
+      } else {
+        this.socket.emit("errorMessage", {
+          text: "/join requires a room name, i.e. /join kittenluvvers."
+        })
+      }
     } else {
       this.socket.emit("errorMessage", {
         text: "/" + text + " is not a recognized command."
